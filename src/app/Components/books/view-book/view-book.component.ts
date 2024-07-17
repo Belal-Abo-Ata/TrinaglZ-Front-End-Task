00ec7a1bc90156405bcd1779aab664e3c1ed1c8f
Belal Abo Ata <belalbelalmohamed1234@gmail.com>
2024-07-17 17:33:53 +0300

    fix: delete on view page

--- src/app/Components/books/view-book/view-book.component.ts
+++ src/app/Components/books/view-book/view-book.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { BooksService } from '../../../Services/books.service';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import BookInterface from '../../../Interfaces/books.interface';
 import { DatePipe } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { DeleteModalComponent } from '../../delete-modal/delete-modal.component';
 
 @Component({
 	selector: 'app-view-book',
@@ -17,14 +19,17 @@ export class ViewBookComponent implements OnInit {
 	constructor(
 		private bookService: BooksService,
 		private router: ActivatedRoute,
+		private dialog: MatDialog,
+		private route: Router,
 	) {}
+
 	ngOnInit(): void {
 		this.ISBN = this.router.snapshot.params['isbn'];
 		this.bookService.getBook(this.ISBN).subscribe({
 			next: (res) => {
 				const books = res as BookInterface[];
 				this.book = books[0];
-        console.log(this.book);
+				console.log(this.book);
 			},
 			error: (err) => {
 				console.log('Get Book Request Failed', err);
@@ -34,4 +39,16 @@ export class ViewBookComponent implements OnInit {
 
 	ISBN!: string;
 	book!: Partial<BookInterface>;
+
+	openModal(): void {
+		const dialogRef = this.dialog.open(DeleteModalComponent, {
+			width: '600px',
+			enterAnimationDuration: '500ms',
+			exitAnimationDuration: '300ms',
+			data: { ISBN: this.ISBN },
+		});
+		dialogRef.afterClosed().subscribe((result) => {
+			this.route.navigate(['/books'], { queryParams: { refresh: 'true' } });
+		});
+	}
 }
